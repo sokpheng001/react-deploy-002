1bb6369e5966f9c9a533a176e71db88b4ed7eaaf
sokpheng001 <kimchansokpheng123@gmail.com>
2025-04-08 09:48:32 +0700

    update for vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+import { Helmet } from "react-helmet";
 import "./App.css";
 
 function App() {
   return (
-    <main className="flex justify-center h-screen flex-col items-center">
-      <p className="text-8xl">Home</p>
-    </main>
+    <>
+      <Helmet>
+        <title>Home | My React App</title>
+        <meta
+          name="description"
+          content="Welcome to the homepage of My React App."
+        />
+
+        {/* Open Graph (Facebook, LinkedIn, etc.) */}
+        <meta property="og:title" content="Welcome to My React App" />
+        <meta
+          property="og:description"
+          content="Explore the best React content here."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://yourdomain.com/" />
+        <meta
+          property="og:image"
+          content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh3Z0RTY1Ot7U5GQWr7uwjFX_HMZB85SZ9EA&s"
+        />
+      </Helmet>
+      <main className="flex justify-center h-screen flex-col items-center">
+        <p className="text-8xl">Home</p>
+      </main>
+    </>
   );
 }
 
